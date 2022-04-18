46ed349ab4f4a07f16f89c412c682aeddfc15de9
imase <imase0730@gmail.com>
2022-04-18 10:05:51 +0900

    テクスチャ（サンプラー使用）

--- PixelShader.hlsl
+++ PixelShader.hlsl
@@ -2,12 +2,5 @@
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	float4 color;
-	int width, height;
-
-	ColorMap.GetDimensions(width, height);
-	float3 texCoord3 = float3(input.TexCoord.x * width, input.TexCoord.y * height, 0);
-	color = ColorMap.Load(texCoord3);
-
-	return color;
+	return ColorMap.Sample(ColorSmp, input.TexCoord);
 }
